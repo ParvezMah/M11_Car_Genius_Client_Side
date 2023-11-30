43b9900534121297ddcee38f9469516654d82ae4
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2023-11-30 13:11:52 +0600

    project Started and Banner Added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+
+
+const Home = () => {
+    return (
+        <div className="my-4">
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
