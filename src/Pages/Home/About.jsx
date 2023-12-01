d179b3ffe6650f2277fa117716c1c1ab56fc4322
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2023-12-01 09:39:47 +0600

    About Section Added

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -1,12 +1,18 @@
+import person from '../../../public/images/about_us/person.jpg'
+import parts from '../../../public/images/about_us/parts.jpg'
+
 const About = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-            <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <div className='lg:w-1/2 relative'>
+                <img src={person} className='max-w-sm rounded-lg shadow-2xl' />
+                <img src={parts} className='absolute border-2 top-44 left-44 max-w-sm rounded-lg shadow-2xl' />
+            </div>
+            <div className='lg:w-1/2'>
+                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button className="btn btn-primary">Get Started</button>
             </div>
         </div>
 </div>
